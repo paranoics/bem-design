ec60114b7ff63953ae8df04f1c88e3c2716d4ca5
Михаил О. Колосков <meeq@yamoney.ru>
2017-11-06 13:14:03 +0300

    bundles rebuild

---
+++ article-icons/article-icons.deps.js
@@ -0,0 +1,347 @@
+exports.deps = [
+    {
+        "block": "page"
+    },
+    {
+        "block": "i-bem-dom"
+    },
+    {
+        "block": "inherit"
+    },
+    {
+        "block": "jquery"
+    },
+    {
+        "block": "loader"
+    },
+    {
+        "block": "loader",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "config"
+    },
+    {
+        "block": "ua"
+    },
+    {
+        "block": "objects"
+    },
+    {
+        "block": "functions"
+    },
+    {
+        "block": "dom"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "init"
+    },
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "identify"
+    },
+    {
+        "block": "next-tick"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "events"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "collection"
+    },
+    {
+        "block": "i-bem",
+        "elem": "collection"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "events",
+        "mod": "type"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "events",
+        "mod": "type",
+        "val": "bem"
+    },
+    {
+        "block": "events"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "init",
+        "mod": "auto"
+    },
+    {
+        "block": "page",
+        "elem": "css"
+    },
+    {
+        "block": "page",
+        "elem": "js"
+    },
+    {
+        "block": "page",
+        "mod": "theme"
+    },
+    {
+        "block": "page",
+        "mod": "theme",
+        "val": "islands"
+    },
+    {
+        "block": "ua",
+        "elem": "svg"
+    },
+    {
+        "block": "page",
+        "elem": "meta"
+    },
+    {
+        "block": "theme"
+    },
+    {
+        "block": "theme",
+        "mod": "color"
+    },
+    {
+        "block": "theme",
+        "mod": "color",
+        "val": "default"
+    },
+    {
+        "block": "theme",
+        "mod": "space"
+    },
+    {
+        "block": "theme",
+        "mod": "space",
+        "val": "default"
+    },
+    {
+        "block": "theme",
+        "mod": "size"
+    },
+    {
+        "block": "theme",
+        "mod": "size",
+        "val": "default"
+    },
+    {
+        "block": "demo"
+    },
+    {
+        "block": "demo",
+        "elem": "header"
+    },
+    {
+        "block": "link"
+    },
+    {
+        "block": "link",
+        "mod": "disabled"
+    },
+    {
+        "block": "jquery",
+        "elem": "event"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerclick"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointernative"
+    },
+    {
+        "block": "keyboard",
+        "elem": "codes"
+    },
+    {
+        "block": "control"
+    },
+    {
+        "block": "demo",
+        "elem": "menu"
+    },
+    {
+        "block": "text"
+    },
+    {
+        "block": "text",
+        "mod": "view"
+    },
+    {
+        "block": "text",
+        "mod": "size"
+    },
+    {
+        "block": "demo",
+        "elem": "control-panel"
+    },
+    {
+        "block": "demo",
+        "elem": "content"
+    },
+    {
+        "block": "b-page"
+    },
+    {
+        "block": "b-page",
+        "elem": "content"
+    },
+    {
+        "block": "vectors"
+    },
+    {
+        "block": "demo",
+        "elem": "content",
+        "mod": "size"
+    },
+    {
+        "block": "text",
+        "mod": "size",
+        "val": "xxxl"
+    },
+    {
+        "block": "text",
+        "mod": "weight"
+    },
+    {
+        "block": "text",
+        "mod": "weight",
+        "val": "bold"
+    },
+    {
+        "block": "decorator"
+    },
+    {
+        "block": "decorator",
+        "mod": "indent-b"
+    },
+    {
+        "block": "decorator",
+        "mod": "indent-b",
+        "val": "xl"
+    },
+    {
+        "block": "text",
+        "mod": "view",
+        "val": "primary"
+    },
+    {
+        "block": "text",
+        "mod": "size",
+        "val": "xl"
+    },
+    {
+        "block": "text",
+        "mod": "type"
+    },
+    {
+        "block": "text",
+        "mod": "type",
+        "val": "h2"
+    },
+    {
+        "block": "text",
+        "mod": "type",
+        "val": "p"
+    },
+    {
+        "block": "image"
+    },
+    {
+        "block": "image",
+        "mod": "view"
+    },
+    {
+        "block": "image",
+        "mod": "view",
+        "val": "icon-grid"
+    },
+    {
+        "block": "image",
+        "mod": "view",
+        "val": "icon-form"
+    },
+    {
+        "block": "image",
+        "mod": "view",
+        "val": "icon-size"
+    },
+    {
+        "block": "image",
+        "mod": "view",
+        "val": "icon-sizes"
+    },
+    {
+        "block": "text",
+        "mod": "view",
+        "val": "ghost"
+    },
+    {
+        "block": "loader",
+        "mod": "type",
+        "val": "js"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "events",
+        "mod": "type",
+        "val": "dom"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "init",
+        "mod": "auto",
+        "val": true
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointer"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerpressrelease"
+    },
+    {
+        "block": "text",
+        "mod": "view",
+        "val": "inverse"
+    },
+    {
+        "block": "text",
+        "mod": "size",
+        "val": "m"
+    },
+    {
+        "block": "demo",
+        "elem": "content",
+        "mod": "size",
+        "val": "s"
+    }
+];
