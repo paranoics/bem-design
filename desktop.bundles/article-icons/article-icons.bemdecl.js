09f6bc52582a34588288546d40bd87d18d48a083
Михаил О. Колосков <meeq@yamoney.ru>
2017-11-12 00:09:32 +0300

    ben-design pages

---
+++ desktop.bundles/article-icons/article-icons.bemdecl.js
@@ -0,0 +1,302 @@
+exports.blocks = [
+    {
+        "name": "page"
+    },
+    {
+        "name": "page",
+        "mods": [
+            {
+                "name": "theme",
+                "vals": [
+                    {
+                        "name": "islands"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "meta"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "css"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "js"
+            }
+        ]
+    },
+    {
+        "name": "theme"
+    },
+    {
+        "name": "theme",
+        "mods": [
+            {
+                "name": "color",
+                "vals": [
+                    {
+                        "name": "default"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "theme",
+        "mods": [
+            {
+                "name": "space",
+                "vals": [
+                    {
+                        "name": "default"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "theme",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "default"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "demo"
+    },
+    {
+        "name": "b-page",
+        "elems": [
+            {
+                "name": "content"
+            }
+        ]
+    },
+    {
+        "name": "vectors"
+    },
+    {
+        "name": "demo",
+        "elems": [
+            {
+                "name": "content"
+            }
+        ]
+    },
+    {
+        "name": "demo",
+        "elems": [
+            {
+                "name": "content",
+                "mods": [
+                    {
+                        "name": "size",
+                        "vals": [
+                            {
+                                "name": "s"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text"
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "xxxl"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "weight",
+                "vals": [
+                    {
+                        "name": "bold"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "decorator"
+    },
+    {
+        "name": "decorator",
+        "mods": [
+            {
+                "name": "indent-b",
+                "vals": [
+                    {
+                        "name": "xl"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "primary"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "xl"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "type",
+                "vals": [
+                    {
+                        "name": "h2"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "m"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "type",
+                "vals": [
+                    {
+                        "name": "p"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "image"
+    },
+    {
+        "name": "image",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "icon-grid"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "image",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "icon-form"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "image",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "icon-size"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "image",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "icon-sizes"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "ghost"
+                    }
+                ]
+            }
+        ]
+    }
+];
---
+++ desktop.bundles/typo/typo.bemdecl.js
@@ -0,0 +1,511 @@
+exports.blocks = [
+    {
+        "name": "page"
+    },
+    {
+        "name": "page",
+        "mods": [
+            {
+                "name": "type",
+                "vals": [
+                    {
+                        "name": "full-screen"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "mods": [
+            {
+                "name": "theme",
+                "vals": [
+                    {
+                        "name": "islands"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "mods": [
+            {
+                "name": "vertical-align",
+                "vals": [
+                    {
+                        "name": "center"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "meta"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "css"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "js"
+            }
+        ]
+    },
+    {
+        "name": "theme"
+    },
+    {
+        "name": "theme",
+        "mods": [
+            {
+                "name": "color",
+                "vals": [
+                    {
+                        "name": "default"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "theme",
+        "mods": [
+            {
+                "name": "space",
+                "vals": [
+                    {
+                        "name": "default"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "theme",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "default"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "demo"
+    },
+    {
+        "name": "b-page",
+        "elems": [
+            {
+                "name": "content"
+            }
+        ]
+    },
+    {
+        "name": "demo",
+        "elems": [
+            {
+                "name": "content"
+            }
+        ]
+    },
+    {
+        "name": "text"
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "xxxl"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "weight",
+                "vals": [
+                    {
+                        "name": "bold"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "decorator"
+    },
+    {
+        "name": "decorator",
+        "mods": [
+            {
+                "name": "indent-b",
+                "vals": [
+                    {
+                        "name": "xl"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "xxl"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "primary"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "decorator",
+        "mods": [
+            {
+                "name": "indent-b",
+                "vals": [
+                    {
+                        "name": "xxxxl"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "demo",
+        "elems": [
+            {
+                "name": "section"
+            }
+        ]
+    },
+    {
+        "name": "row"
+    },
+    {
+        "name": "row",
+        "elems": [
+            {
+                "name": "col"
+            }
+        ]
+    },
+    {
+        "name": "row",
+        "elems": [
+            {
+                "name": "col",
+                "mods": [
+                    {
+                        "name": "sw",
+                        "vals": [
+                            {
+                                "name": 4
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "typography"
+    },
+    {
+        "name": "typography",
+        "elems": [
+            {
+                "name": "item"
+            }
+        ]
+    },
+    {
+        "name": "typography",
+        "elems": [
+            {
+                "name": "view"
+            }
+        ]
+    },
+    {
+        "name": "typography",
+        "elems": [
+            {
+                "name": "text"
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "m"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "xl"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "l"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "s"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "size",
+                "vals": [
+                    {
+                        "name": "xs"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "promo"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "secondary"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "ghost"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "inverse"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "income"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "letter"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "disable"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "link"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "view",
+                "vals": [
+                    {
+                        "name": "link-minor"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "transform",
+                "vals": [
+                    {
+                        "name": "uppercase"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "spacing",
+                "vals": [
+                    {
+                        "name": "s"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "spacing",
+                "vals": [
+                    {
+                        "name": "m"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "text",
+        "mods": [
+            {
+                "name": "style",
+                "vals": [
+                    {
+                        "name": "italic"
+                    }
+                ]
+            }
+        ]
+    }
+];
